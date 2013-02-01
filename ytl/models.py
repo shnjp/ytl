b872457d5997a171d119ce2a0b502e0a3c51e945
Shin ADACHI <shn@glucose.jp>
2013-02-02 01:54:59 +0900

    digg google imageの修正

--- ytl/models.py
+++ ytl/models.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 from flask.ext.mongokit import Document
+import datetime
 
 
 class Photo(Document):
     __collection__ = 'photos'
+    # TODO:めんどい
+    use_schemaless = True
+    structure = {
+        'reblog_key': unicode,
+        'posts': [basestring]
+    }
+    required_fields = ['reblog_key', 'posts']
 
 
 class Blog(Document):
     __collection__ = 'blogs'
 
+    structure = {
+        'blog_name': basestring,
+        'date_fetch_by_google': datetime.datetime
+    }
+    required_fields = ['blog_name']
 
 __all_classes__ = [Photo, Blog]
